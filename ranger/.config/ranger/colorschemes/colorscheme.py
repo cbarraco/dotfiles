e867f5b18179f9363368531c04af2ea7b52a6509
cjbarrac <carlobarraco@gmail.com>
2015-03-14 11:31:32 -0400

    Added cmus

--- ranger/.config/ranger/colorschemes/colorscheme.py
+++ ranger/.config/ranger/colorschemes/colorscheme.py
@@ -2,7 +2,7 @@ from ranger.gui.colorscheme import ColorScheme
 from ranger.gui.color import *
 
 class Default(ColorScheme):
-    progress_bar_color = 8
+    progress_bar_color = 2
     def use(self, context):
         fg, bg, attr = default_colors
         if context.reset:
