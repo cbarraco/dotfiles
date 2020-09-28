79aeca5b8a40e2026a74347157944e9b519d6cf6
Carlo Barraco <carlobarraco@gmail.com>
2020-09-28 15:41:39 -0400

    Pruning install script

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -1,14 +1,8 @@
-# install ppas
-add-apt-repository ppa:bashtop-monitor/bashtop
-apt-get update
-
 # install dev tools
-apt-get install -y build-essential git autoconf cmake golang python3-pip nodejs icecream
+apt-get install -y build-essential git autoconf cmake python3-pip nodejs icecream
 
 # install network tools
 apt-get install -y wget nmap curl
 
 # install tui programs
-apt-get install -y ncdu bashtop htop powertop taskwarrior
-go get -u github.com/gokcehan/lf
-npm i -g termllo
+apt-get install -y ncdu htop powertop ranger tig
